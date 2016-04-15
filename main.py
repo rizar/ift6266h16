4cff6cecb6f11dd2b7baa9b43e321997a1533fc6
rizar <dimabv@tut.by>
2016-04-15 16:27:35 -0400

    Force minimum dimension and use server

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 """Convolutional network example.
 
 Run the training for 50 epochs with
@@ -12,6 +13,7 @@ from __future__ import print_function
 import logging
 import numpy
 import os
+import math
 from argparse import ArgumentParser
 
 import theano
@@ -37,13 +39,65 @@ from blocks.model import Model
 from blocks.monitoring import aggregation
 from fuel.datasets import DogsVsCats
 from fuel.schemes import ShuffledScheme, SequentialExampleScheme
-from fuel.streams import DataStream
+from fuel.streams import DataStream, ServerDataStream
 from fuel.transformers import ScaleAndShift, ForceFloatX
 from fuel.transformers.image import (
-    MinimumImageDimensions, RandomFixedSizeCrop)
+    RandomFixedSizeCrop,
+    SourcewiseTransformer, ExpectsAxisLabels)
+from fuel.server import start_server
 from toolz.itertoolz import interleave
+from PIL import Image
 
 logger = logging.getLogger(__name__)
+
+
+class ForceMinimumDimension(SourcewiseTransformer, ExpectsAxisLabels):
+    def __init__(self, data_stream, min_dim, resample='nearest',
+                 **kwargs):
+        self.min_dim = min_dim
+        try:
+            self.resample = getattr(Image, resample.upper())
+        except AttributeError:
+            raise ValueError("unknown resampling filter '{}'".format(resample))
+        kwargs.setdefault('produces_examples', data_stream.produces_examples)
+        kwargs.setdefault('axis_labels', data_stream.axis_labels)
+        super(ForceMinimumDimension, self).__init__(data_stream, **kwargs)
+
+    def transform_source_batch(self, batch, source_name):
+        self.verify_axis_labels(('batch', 'channel', 'height', 'width'),
+                                self.data_stream.axis_labels[source_name],
+                                source_name)
+        return [self._example_transform(im, source_name) for im in batch]
+
+    def transform_source_example(self, example, source_name):
+        self.verify_axis_labels(('channel', 'height', 'width'),
+                                self.data_stream.axis_labels[source_name],
+                                source_name)
+        return self._example_transform(example, source_name)
+
+    def _example_transform(self, example, _):
+        if example.ndim > 3 or example.ndim < 2:
+            raise NotImplementedError
+        original_min_dim = min(example.shape[-2:])
+        multiplier = self.min_dim / float(original_min_dim)
+        dt = example.dtype
+        # If we're dealing with a colour image, swap around the axes
+        # to be in the format that PIL needs.
+        if example.ndim == 3:
+            im = example.transpose(1, 2, 0)
+        else:
+            im = example
+        im = Image.fromarray(im)
+        width, height = im.size
+        width = int(math.ceil(width * multiplier))
+        height = int(math.ceil(height * multiplier))
+        im = numpy.array(im.resize((width, height))).astype(dt)
+        # If necessary, undo the axis swap from earlier.
+        if im.ndim == 3:
+            example = im.transpose(2, 0, 1)
+        else:
+            example = im
+        return example
 
 
 class LeNet(FeedforwardSequence, Initializable):
@@ -158,8 +212,8 @@ class LeNet(FeedforwardSequence, Initializable):
 
 
 def add_transformers(stream, random_crop=False):
-    stream = MinimumImageDimensions(stream, (128, 128),
-                                    which_sources=['image_features'])
+    stream = ForceMinimumDimension(stream, 128,
+                                   which_sources=['image_features'])
     if random_crop:
         stream = RandomFixedSizeCrop(stream, (128, 128),
                                     which_sources=['image_features'])
@@ -244,20 +298,24 @@ def main(mode, save_to, num_epochs, load_params=None, feature_maps=None, mlp_hid
         with open(load_params, 'r') as src:
             model.set_parameter_values(load_parameters(src))
 
+    # Training stream with random cropping
+    train = DogsVsCats(("train",), subset=slice(None, 25000 - valid_examples, None))
+    train_str =  DataStream(
+        train, iteration_scheme=ShuffledScheme(train.num_examples, batch_size))
+    train_str = add_transformers(train_str, random_crop=True)
+
+    # Validation stream without cropping
+    valid = DogsVsCats(("train",), subset=slice(25000 - valid_examples, None, None))
+    valid_str = DataStream(
+        valid, iteration_scheme=SequentialExampleScheme(valid.num_examples))
+    valid_str = add_transformers(valid_str)
+
+    if mode == 'server':
+        start_server(train_str)
     if mode == 'train':
+        train_str = ServerDataStream(('image_features', 'targets'), produces_examples=False)
+
         save_to_base, save_to_extension = os.path.splitext(save_to)
-
-        # Training stream with random cropping
-        train = DogsVsCats(("train",), subset=slice(None, 25000 - valid_examples, None))
-        train_str =  DataStream(
-            train, iteration_scheme=ShuffledScheme(train.num_examples, batch_size))
-        train_str = add_transformers(train_str, random_crop=True)
-
-        # Validation stream without cropping
-        valid = DogsVsCats(("train",), subset=slice(25000 - valid_examples, None, None))
-        valid_str = DataStream(
-            train, iteration_scheme=SequentialExampleScheme(valid.num_examples))
-        valid_str = add_transformers(valid_str)
 
         # Train with simple SGD
         if algo == 'rmsprop':
@@ -329,7 +387,7 @@ if __name__ == "__main__":
     parser = ArgumentParser("An example of training a convolutional network "
                             "on the MNIST dataset.")
     parser.add_argument("mode",
-                        help="What to do", choices=['train', 'test'])
+                        help="What to do", choices=['train', 'server', 'test'])
     parser.add_argument("save_to",
                         help="Destination to save the state of the training "
                              "process.")
